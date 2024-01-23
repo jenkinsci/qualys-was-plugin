ff6164310db0987e1c46467e579bb8016e67b1a8
Rushikesh Dengale <rdengale@qualys.com>
2024-01-23 14:57:45 +0530

    Fixed ui for post build action,added changes for KB data pull

--- src/main/java/com/qualys/plugins/wasPlugin/util/Helper.java
+++ src/main/java/com/qualys/plugins/wasPlugin/util/Helper.java
@@ -45,207 +45,180 @@ import hudson.util.ListBoxModel.Option;
 
 public class Helper {
 	public static String TEST_CONNECTION_PATH = "/qps/rest/3.0/count/was/webapp";
-    public static String LAUNCH_SCAN_PATH = "/qps/rest/3.0/launch/was/wasscan";
-    public static String WEBAPP_DETAILS_PATH = "/qps/rest/3.0/get/was/%s/%s";
-    public static String SCAN_STATUS_PATH = "/qps/rest/3.0/get/was/wasscan/%s";
-    public static String SCAN_RESULT_PATH = "/qps/rest/3.0/download/was/wasscan/%s";
-    
-    public static final Map<String, Map<String, String>> platformsList;
-    static {
-    	Map<String, Map<String, String>> aList = new LinkedHashMap<String,Map<String, String>>();
-    	
-    	Map<String, String> platform1 = new HashMap <String, String>();
-    	platform1.put("name", "US Platform 1"); platform1.put("code", "US_PLATFORM_1"); platform1.put("url", "https://qualysapi.qualys.com");
-    	platform1.put("portal", "https://qualysguard.qualys.com"); aList.put("US_PLATFORM_1", platform1);
-    	
-    	Map<String, String> platform2 = new HashMap <String, String>();
-    	platform2.put("name", "US Platform 2"); platform2.put("code", "US_PLATFORM_2"); platform2.put("url", "https://qualysapi.qg2.apps.qualys.com"); 
-    	platform2.put("portal", "https://qualysguard.qg2.apps.qualys.com"); aList.put("US_PLATFORM_2", platform2);
-    	
-    	Map<String, String> platform3 = new HashMap <String, String>();
-    	platform3.put("name", "US Platform 3"); platform3.put("code", "US_PLATFORM_3"); platform3.put("url", "https://qualysapi.qg3.apps.qualys.com"); 
-    	platform3.put("portal", "https://qualysguard.qg3.apps.qualys.com"); aList.put("US_PLATFORM_3", platform3);
-    	
-    	Map<String, String> platform4 = new HashMap <String, String>();
-    	platform4.put("name", "EU Platform 1"); platform4.put("code", "EU_PLATFORM_1"); platform4.put("url", "https://qualysapi.qualys.eu");
-    	platform4.put("portal", "https://qualysguard.qualys.eu"); aList.put("EU_PLATFORM_1", platform4);
-    	
-    	Map<String, String> platform5 = new HashMap <String, String>();
-    	platform5.put("name", "EU Platform 2"); platform5.put("code", "EU_PLATFORM_2"); platform5.put("url", "https://qualysapi.qg2.apps.qualys.eu");
-    	platform5.put("portal", "https://qualysguard.qg2.apps.qualys.eu"); aList.put("EU_PLATFORM_2", platform5);
-    	
-    	Map<String, String> platform6 = new HashMap <String, String>();
-    	platform6.put("name", "INDIA Platform"); platform6.put("code", "INDIA_PLATFORM"); platform6.put("url", "https://qualysapi.qg1.apps.qualys.in");
-    	platform6.put("portal", "https://qualysguard.qg1.apps.qualys.in"); aList.put("INDIA_PLATFORM", platform6);
+	public static String LAUNCH_SCAN_PATH = "/qps/rest/3.0/launch/was/wasscan";
+	public static String WEBAPP_DETAILS_PATH = "/qps/rest/3.0/get/was/%s/%s";
+	public static String SCAN_STATUS_PATH = "/qps/rest/3.0/get/was/wasscan/%s";
+	public static String SCAN_RESULT_PATH = "/qps/rest/3.0/download/was/wasscan/%s";
 
-    	Map<String, String> platform7 = new HashMap <String, String>();
-    	platform7.put("name", "CANADA Platform"); platform7.put("code", "CANADA_PLATFORM"); platform7.put("url", "https://qualysapi.qg1.apps.qualys.ca");
-    	platform7.put("portal", "https://qualysguard.qg1.apps.qualys.ca"); aList.put("CANADA_PLATFORM", platform7);
-    	
-    	Map<String, String> platform8 = new HashMap <String, String>();
-    	platform8.put("name", "AE Platform"); platform8.put("code", "AE_PLATFORM"); platform8.put("url", "https://qualysapi.qg1.apps.qualys.ae");
-    	platform8.put("portal", "https://qualysguard.qg1.apps.qualys.ae"); aList.put("AE_PLATFORM", platform8);
-    		
-    	Map<String, String> platform9 = new HashMap <String, String>();
-    	platform9.put("name", "Private Cloud Platform"); platform9.put("code", "PCP"); platform9.put("url", "");
-    	aList.put("PCP", platform9);
-    	
-    	platformsList = Collections.unmodifiableMap(aList);
-    }
-	
-    private final static Logger logger = Logger.getLogger(Helper.class.getName());
-    
-	public static String secondsToReadableTime(long duration){
-	    long days = TimeUnit.SECONDS.toDays(duration);
-	    duration -= TimeUnit.DAYS.toSeconds(days);
+	public static final Map<String, Map<String, String>> platformsList;
+	static {
+		Map<String, Map<String, String>> aList = new LinkedHashMap<String,Map<String, String>>();
 
-	    long hours = TimeUnit.SECONDS.toHours(duration);
-	    duration -= TimeUnit.HOURS.toSeconds(hours);
+		Map<String, String> platform1 = new HashMap <String, String>();
+		platform1.put("name", "US Platform 1"); platform1.put("code", "US_PLATFORM_1"); platform1.put("url", "https://qualysapi.qualys.com");
+		platform1.put("portal", "https://qualysguard.qualys.com"); aList.put("US_PLATFORM_1", platform1);
 
-	    long minutes = TimeUnit.SECONDS.toMinutes(duration);
-	    duration -= TimeUnit.MINUTES.toSeconds(minutes);
+		Map<String, String> platform2 = new HashMap <String, String>();
+		platform2.put("name", "US Platform 2"); platform2.put("code", "US_PLATFORM_2"); platform2.put("url", "https://qualysapi.qg2.apps.qualys.com");
+		platform2.put("portal", "https://qualysguard.qg2.apps.qualys.com"); aList.put("US_PLATFORM_2", platform2);
 
-	    long seconds = TimeUnit.SECONDS.toSeconds(duration);
+		Map<String, String> platform3 = new HashMap <String, String>();
+		platform3.put("name", "US Platform 3"); platform3.put("code", "US_PLATFORM_3"); platform3.put("url", "https://qualysapi.qg3.apps.qualys.com");
+		platform3.put("portal", "https://qualysguard.qg3.apps.qualys.com"); aList.put("US_PLATFORM_3", platform3);
 
-	    StringBuilder msg = new StringBuilder( );
-	    if (days!=0) {
-	        msg.append( days+"day(s) ");
-	    }
-	    if (hours!=0) {
-	        msg.append( hours+" hr ");
-	    }
-	    if (minutes!=0) {
-	        msg.append( minutes+" min ");
-	    }
-	    if (seconds!=0) {
-	        msg.append( seconds+" s ");
-	    }
-	    return msg.toString();
+		Map<String, String> platform4 = new HashMap <String, String>();
+		platform4.put("name", "EU Platform 1"); platform4.put("code", "EU_PLATFORM_1"); platform4.put("url", "https://qualysapi.qualys.eu");
+		platform4.put("portal", "https://qualysguard.qualys.eu"); aList.put("EU_PLATFORM_1", platform4);
+
+		Map<String, String> platform5 = new HashMap <String, String>();
+		platform5.put("name", "EU Platform 2"); platform5.put("code", "EU_PLATFORM_2"); platform5.put("url", "https://qualysapi.qg2.apps.qualys.eu");
+		platform5.put("portal", "https://qualysguard.qg2.apps.qualys.eu"); aList.put("EU_PLATFORM_2", platform5);
+
+		Map<String, String> platform6 = new HashMap <String, String>();
+		platform6.put("name", "INDIA Platform"); platform6.put("code", "INDIA_PLATFORM"); platform6.put("url", "https://qualysapi.qg1.apps.qualys.in");
+		platform6.put("portal", "https://qualysguard.qg1.apps.qualys.in"); aList.put("INDIA_PLATFORM", platform6);
+
+		Map<String, String> platform7 = new HashMap <String, String>();
+		platform7.put("name", "CANADA Platform"); platform7.put("code", "CANADA_PLATFORM"); platform7.put("url", "https://qualysapi.qg1.apps.qualys.ca");
+		platform7.put("portal", "https://qualysguard.qg1.apps.qualys.ca"); aList.put("CANADA_PLATFORM", platform7);
+
+		Map<String, String> platform8 = new HashMap <String, String>();
+		platform8.put("name", "AE Platform"); platform8.put("code", "AE_PLATFORM"); platform8.put("url", "https://qualysapi.qg1.apps.qualys.ae");
+		platform8.put("portal", "https://qualysguard.qg1.apps.qualys.ae"); aList.put("AE_PLATFORM", platform8);
+
+		Map<String, String> platform9 = new HashMap <String, String>();
+		platform9.put("name", "Private Cloud Platform"); platform9.put("code", "PCP"); platform9.put("url", "");
+		aList.put("PCP", platform9);
+
+		platformsList = Collections.unmodifiableMap(aList);
 	}
-	
-	public static Object execXPath(String xml, String xPath, QName type) {
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	private final static Logger logger = Logger.getLogger(Helper.class.getName());
 
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder
-                    .parse(new InputSource(new StringReader(xml)));
-            return execXPath(doc, xPath, type);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	public static String secondsToReadableTime(long duration){
+		long days = TimeUnit.SECONDS.toDays(duration);
+		duration -= TimeUnit.DAYS.toSeconds(days);
 
-        return null;
-    }
-    public static Object execXPath(Node node, String xPath, QName type) {
+		long hours = TimeUnit.SECONDS.toHours(duration);
+		duration -= TimeUnit.HOURS.toSeconds(hours);
 
-        XPath xpath = XPathFactory.newInstance().newXPath();
+		long minutes = TimeUnit.SECONDS.toMinutes(duration);
+		duration -= TimeUnit.MINUTES.toSeconds(minutes);
 
-        Object object = null;
-        try {
-            object = xpath.evaluate(xPath, node, type);
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
-        return object;
-    }
-    
-    public static Logger getLogger(String loggerName) {
-    	Logger pluginLogger = Logger.getLogger(loggerName);
-    	try {
-    		pluginLogger.setUseParentHandlers(false);
-	    	QualysLogFormatter formatter = new QualysLogFormatter();
-	    	Handler handler = new ConsoleHandler();
-	    	handler.setFormatter(formatter);
-	    	pluginLogger.addHandler(handler);
-    	} catch(Exception e) {
-    		logger.info("Error while formatting logger, reason: " + e.getMessage());
-    	}
-    	return pluginLogger;
-    }
-    
-    public static class QualysLogFormatter extends Formatter {
-        // Create a DateFormat to format the logger timestamp.
-        private static final DateFormat df = new SimpleDateFormat("MMM dd, yyyy hh:mm:ss a");
+		long seconds = TimeUnit.SECONDS.toSeconds(duration);
 
-        public String format(LogRecord record) {
-            StringBuilder builder = new StringBuilder(1000);
-            builder.append(df.format(new Date(record.getMillis()))).append(" - ");
-            builder.append("[").append(record.getSourceClassName()).append(".");
-            builder.append(record.getSourceMethodName()).append("] - ");
-            builder.append(record.getLevel()).append(": ");
-            builder.append(formatMessage(record));
-            builder.append("\n");
-            return builder.toString();
-        }
+		StringBuilder msg = new StringBuilder( );
+		if (days!=0) {
+			msg.append( days+"day(s) ");
+		}
+		if (hours!=0) {
+			msg.append( hours+" hr ");
+		}
+		if (minutes!=0) {
+			msg.append( minutes+" min ");
+		}
+		if (seconds!=0) {
+			msg.append( seconds+" s ");
+		}
+		return msg.toString();
+	}
 
-        public String getHead(Handler h) {
-            return super.getHead(h);
-        }
 
-        public String getTail(Handler h) {
-            return super.getTail(h);
-        }
-    }
+	public static Logger getLogger(String loggerName) {
+		Logger pluginLogger = Logger.getLogger(loggerName);
+		try {
+			pluginLogger.setUseParentHandlers(false);
+			QualysLogFormatter formatter = new QualysLogFormatter();
+			Handler handler = new ConsoleHandler();
+			handler.setFormatter(formatter);
+			pluginLogger.addHandler(handler);
+		} catch(Exception e) {
+			logger.info("Error while formatting logger, reason: " + e.getMessage());
+		}
+		return pluginLogger;
+	}
+
+	public static class QualysLogFormatter extends Formatter {
+		// Create a DateFormat to format the logger timestamp.
+		private static final DateFormat df = new SimpleDateFormat("MMM dd, yyyy hh:mm:ss a");
+
+		public String format(LogRecord record) {
+			StringBuilder builder = new StringBuilder(1000);
+			builder.append(df.format(new Date(record.getMillis()))).append(" - ");
+			builder.append("[").append(record.getSourceClassName()).append(".");
+			builder.append(record.getSourceMethodName()).append("] - ");
+			builder.append(record.getLevel()).append(": ");
+			builder.append(formatMessage(record));
+			builder.append("\n");
+			return builder.toString();
+		}
+
+		public String getHead(Handler h) {
+			return super.getHead(h);
+		}
+
+		public String getTail(Handler h) {
+			return super.getTail(h);
+		}
+	}
 
 	public static void createNewFile(String rootDir, String filename, String content, PrintStream buildLogger) {
-  	  	   	
-    	File f = new File(rootDir + File.separator + filename + ".json");
-	    if(!f.getParentFile().exists()){
-	        f.getParentFile().mkdirs();
-	    }
 
-	    if(!f.exists()){
-	        try {
-	            f.createNewFile();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            buildLogger.println("Failed creating file " + filename + ", reason =" + e.getMessage());
-	        }
-	    }
-	    try {
-	        File dir = new File(f.getParentFile(), f.getName());
-	        PrintWriter writer = new PrintWriter(dir);
-	        writer.print(content);
-	        writer.close();
-	    } catch (FileNotFoundException e) {
-	    	e.printStackTrace();
-	    	buildLogger.println("Failed writing to file " + filename + ", reason =" + e.getMessage());
-	    }
-    }
-	
+		File f = new File(rootDir + File.separator + filename + ".json");
+		if(!f.getParentFile().exists()){
+			f.getParentFile().mkdirs();
+		}
+
+		if(!f.exists()){
+			try {
+				f.createNewFile();
+			} catch (Exception e) {
+				e.printStackTrace();
+				buildLogger.println("Failed creating file " + filename + ", reason =" + e.getMessage());
+			}
+		}
+		try {
+			File dir = new File(f.getParentFile(), f.getName());
+			PrintWriter writer = new PrintWriter(dir);
+			writer.print(content);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			buildLogger.println("Failed writing to file " + filename + ", reason =" + e.getMessage());
+		}
+	}
+
 	public static Comparator<Option> OptionItemmsComparator = new Comparator<Option>() {
-        @Override
-        public int compare(Option e1, Option e2) {
-            return e1.name.toLowerCase().compareTo(e2.name.toLowerCase());
-        }
-    };
+		@Override
+		public int compare(Option e1, Option e2) {
+			return e1.name.toLowerCase().compareTo(e2.name.toLowerCase());
+		}
+	};
 
 	public static void copyEvaluationResultToFile(String rootDir, String filename, PrintStream buildLogger,	JsonObject evalresult) throws IOException {
 		Gson gson = new Gson();
 		File f = new File(rootDir + File.separator + filename + ".json");
-		
+
 		if(f.exists()){
-    		String contentStr = FileUtils.readFileToString(f);
-    		JsonObject respObj = gson.fromJson(contentStr, JsonObject.class);
-    		
-    		GsonBuilder builder = new GsonBuilder();
+			String contentStr = FileUtils.readFileToString(f);
+			JsonObject respObj = gson.fromJson(contentStr, JsonObject.class);
+
+			GsonBuilder builder = new GsonBuilder();
 			gson = builder.serializeNulls().create(); // for null values
-			
+
 			String sevVulnsJson = gson.toJson(evalresult);
 			JsonElement sevVulnsElement = gson.fromJson(sevVulnsJson, JsonElement.class);
-    		respObj.add("evaluationResult", sevVulnsElement);
-    		String final_content = gson.toJson(respObj);
-    		try {
-    	        File dir = new File(f.getParentFile(), f.getName());
-    	        PrintWriter writer = new PrintWriter(dir);
-    	        writer.print(final_content);
-    	        writer.close();
-    	    } catch (FileNotFoundException e) {
-    	    	e.printStackTrace();
-    	    	buildLogger.println("Failed writing to file " + filename + ", reason =" + e.getMessage());
-    	    }
-    	}
+			respObj.add("evaluationResult", sevVulnsElement);
+			String final_content = gson.toJson(respObj);
+			try {
+				File dir = new File(f.getParentFile(), f.getName());
+				PrintWriter writer = new PrintWriter(dir);
+				writer.print(final_content);
+				writer.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				buildLogger.println("Failed writing to file " + filename + ", reason =" + e.getMessage());
+			}
+		}
 	}
 }
